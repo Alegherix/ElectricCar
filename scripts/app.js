9e07c12c50fbcc15412134ed52c0247d1b3a1ad6
Alegherix <psao69@gmail.com>
2020-11-15 14:55:34 +0100

    added slide animation when switching carColor

--- scripts/app.js
+++ scripts/app.js
@@ -19,7 +19,21 @@ function changeImage(img) {
     const carImg = document.querySelector(".electricColoringImg");
     const colorRegex = new RegExp("(red)|(yellow)|(green)")
     const buttonColor = img.src.match(colorRegex)[0];
-    carImg.src = carImg.src.replace(colorRegex, buttonColor)
+    // console.log(carImg.classList.toggle("active"));
+
+    const tl = gsap.timeline();
+    tl.to(".electricColoringImg", {
+        opacity: 0,
+        x: 200,
+        duration: 0.5,
+        onComplete: () =>  carImg.src = carImg.src.replace(colorRegex, buttonColor) 
+    })
+    .to(".electricColoringImg", {x:-100, duration: 0.1})
+    .to(".electricColoringImg", {
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+    },)
 }
 
 const main = () => {
