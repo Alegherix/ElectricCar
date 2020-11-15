b258307e1cf18bd271427e90e7e6e02a6ded4158
Alegherix <psao69@gmail.com>
2020-11-15 09:36:45 +0100

    added animations for colorPickingButtons

--- src/animations.js
+++ src/animations.js
@@ -10,20 +10,19 @@ function triggerTextSlide (toAnimate, trigger) {
         stagger: 0.6,
     })
 }
+const animateDescText = () => {
+    triggerTextSlide(".anim1", ".prodDescHeading");
+    triggerTextSlide(".anim2", ".safetyInfoContainer");
+}
 
-const timeLine = gsap.timeline();
-
+const animateHeader = () => {
+    const timeLine = gsap.timeline();
 
 timeLine.from("nav", {opacity: 0, duration: 1, y: -100, ease: Power2.easeOut})
 .from(".logo", {opacity:0, duration: 0.3, y: 50})
 .from(".hamburger", {opacity:0, duration: 0.3, y: -50}, "-=0.3")
 .from(".heroText", {opacity: 0, duration: 0.8, x:-100})
 .from(".heroHeading", {opacity: 0, duration: 1, y:100}, "=-0.7")
-
-
-const animate = () => {
-    triggerTextSlide(".anim1", ".prodDescHeading");
-    triggerTextSlide(".anim2", ".safetyInfoContainer");
 }
 
 const animateColoringTimeline = () => {
@@ -34,9 +33,16 @@ const animateColoringTimeline = () => {
     gsap.from(".electricColoringImg", {
         scrollTrigger: {
         trigger: ".electricColoringImg",
-        start: "20px 65%",
+        start: "140px 90%",
     }, opacity:0, duration: 1.2, y: -100});
+    gsap.from(".anim4", {scrollTrigger: {
+        trigger: ".colorPickerContainer",
+        start:"bottom 100%"
+    }, opacity: 0, duration: 1, stagger: 0.4, rotation: -360, y: 100,
+    ease:Expo.easeOut
+    })    
 }
 
-animate();
+animateHeader();
+animateDescText();
 animateColoringTimeline();
