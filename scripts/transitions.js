e0940ad11b88ef6ffb26add640903b6f208a39ef
Alegherix <psao69@gmail.com>
2020-11-16 08:01:37 +0100

    changed page animation to slidingRight

--- scripts/transitions.js
+++ scripts/transitions.js
@@ -1,8 +1,21 @@
 function pageTransition(){
     const tl = gsap.timeline();
 
-    tl.to("ul.transition li", {duration: .4, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
-    tl.to("ul.transition li", {duration: .3, scaleY: 0, transformOrigin: "bottom left", stagger: .1})
+    tl.to(".loadingScreen", {
+        duration: .8,
+        width: "100%",
+        left: "0%",
+        ease: "Expo.easeInOut"
+    });
+
+    tl.to(".loadingScreen", {
+        duration: .6,
+        width: "100%",
+        left: "100%",
+        ease: "Expo.easeInOut",
+        delay: 0.3
+    });
+    tl.set(".loadingScreen", {left: "-100%"})
 }
 
 function delay(n) {
