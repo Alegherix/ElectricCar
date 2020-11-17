c55a507eac7ddfd12c1b902bb5b0a10655bb2e0d
Alegherix <psao69@gmail.com>
2020-11-17 10:41:57 +0100

    fixed history with new image

---
+++ scripts/historyAnimations.js
@@ -0,0 +1,11 @@
+function animateHistory(){
+    const tl = gsap.timeline();
+    tl.from(".historyAnim", {
+        opacity: 0,
+        x: -100,
+        duration:1.2,
+        stagger: .4
+    })
+}
+
+animateHistory();
