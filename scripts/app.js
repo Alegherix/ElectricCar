92a241bd101c971de1743412f5bf9d7410b9f73c
Alegherix <psao69@gmail.com>
2020-11-18 20:37:49 +0100

    added footer to history, links to ul, and crashed burger

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,40 @@
 const addBurgerListener = () => {
   const burgerButton = document.querySelector('.hamburger');
   burgerButton.addEventListener('click', () => {
-    console.log('Clicked Buurger');
     burgerButton.classList.toggle('is-active');
+    const navigation = document.querySelector('.navlinks');
+    const ulElement = navigation.querySelector('ul');
+    const header = document.querySelector('header');
+
+    if (burgerButton.classList.contains('is-active')) {
+      navigation.style.display = 'flex';
+      navigation.style.backgroundColor = 'var(--houseColor)';
+      navigation.style.position = 'fixed';
+      navigation.style.top = 0;
+      navigation.style.left = 0;
+      navigation.style.height = '100vh';
+      navigation.style.width = '100vw';
+      ulElement.style.flexDirection = 'column';
+      ulElement.style.justifyContent = 'center';
+      ulElement.style.gap = '1rem';
+
+      header.style.width = '100%';
+      header.style.position = 'fixed';
+      header.style.zIndex = 5;
+    } else {
+      navigation.style.display = 'none';
+    }
   });
 };
+
+function enableBurger() {}
+
+function closeBurger() {
+  const burgerButton = document.querySelector('.hamburger');
+  const navigation = document.querySelector('.navlinks');
+  burgerButton.classList.remove('is-active');
+  navigation.style.display = 'none';
+}
 
 // Sätt upp form clearing
 // Samt ett text element som indikerar att formen har submittats
--- scripts/transitions.js
+++ scripts/transitions.js
@@ -1,61 +1,70 @@
-function pageTransition(){
-    const tl = gsap.timeline();
+function pageTransition() {
+  const tl = gsap.timeline();
 
-    tl.to(".loadingScreen", {
-        duration: .8,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut"
-    });
+  tl.to('.loadingScreen', {
+    duration: 0.8,
+    width: '100%',
+    left: '0%',
+    ease: 'Expo.easeInOut',
+  });
 
-    tl.to(".loadingScreen", {
-        duration: .6,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.3
-    });
-    tl.set(".loadingScreen", {left: "-100%"})
+  tl.to('.loadingScreen', {
+    duration: 0.6,
+    width: '100%',
+    left: '100%',
+    ease: 'Expo.easeInOut',
+    delay: 0.3,
+  });
+  tl.set('.loadingScreen', { left: '-100%' });
 }
 
 function delay(n) {
-    n = n || 2000;
-    return new Promise(done => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });  0
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+  0;
 }
 
-function contentAnimation(){
+function contentAnimation() {}
 
+function closeBurgerIfOpen() {
+  const burgerButton = document.querySelector('.hamburger');
+  if (burgerButton.classList.contains('is-active')) {
+    closeBurger();
+  }
 }
 
 barba.init({
-    sync: true,
+  sync: true,
 
-    transitions: [{
-        async leave(data){
-            const done = this.async();
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
 
-            pageTransition();
-            await delay(1200);
-            done();
-        },
+        pageTransition();
+        closeBurgerIfOpen();
+        await delay(1200);
+        done();
+      },
 
-        async enter(data){
-            scrollToTop();
-            animateHeader();
-            // animateHistory();
-        },
+      async enter(data) {
+        scrollToTop();
+        animateHeader();
+        // animateHistory();
+      },
 
-        async once(data){
-            scrollToTop();
-        }
-    }]
-})
+      async once(data) {
+        scrollToTop();
+      },
+    },
+  ],
+});
 
 // Used for fixing page transitions that doesn't move scroll position.
-function scrollToTop(){
-    window.scrollTo(0,0);
+function scrollToTop() {
+  window.scrollTo(0, 0);
 }
