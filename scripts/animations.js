a8ba1c3cdc1f7f2f7eda84c68ae32806303274ed
Alegherix <psao69@gmail.com>
2020-11-25 12:03:47 +0100

    readded animation classes etc

--- scripts/animations.js
+++ scripts/animations.js
@@ -23,7 +23,12 @@ const animateHeader = () => {
     .from('.logo', { opacity: 0, duration: 0.3, y: 50 })
     .from('.hamburger', { opacity: 0, duration: 0.3, y: -50 }, '-=0.3')
     .from('.heroText', { opacity: 0, duration: 0.8, x: -100 })
-    .from('.heroHeading', { opacity: 0, duration: 1, y: 100 }, '=-0.7');
+    .from('.heroHeading', { opacity: 0, duration: 1, y: 100 }, '=-0.7')
+    .from(
+      '.heroSignupContainer',
+      { opacity: 0, duration: 0.6, y: -100 },
+      '=-0.3'
+    );
 };
 
 const animateColoringTimeline = () => {
